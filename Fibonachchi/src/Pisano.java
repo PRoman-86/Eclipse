abda1a5c3aa810e88d2d07b3bdc1ac05c91a6e7a
Roman <82845376+PRoman-86@users.noreply.github.com>
2021-05-14 03:01:07 +0300

    Add files via upload
    fibonachchi refactored

--- Fibonachchi/src/Pisano.java
+++ Fibonachchi/src/Pisano.java
@@ -1,11 +1,21 @@
 public class Pisano {
 	public static void main(String[] args) {
-		long a=1,b=1,c=0;
+		new Pisano(10);
+	}
+
+	Pisano(int val) {
+		long a = 1, b = 1, c = 0;
+		if (val <= 3) {
+			System.out.println("Incorrect value");
+			return;
+		}
+
 		System.out.print("0 1 1 ");
-		for (int i=3; i<15; i++) {
-		c=a+b;
-		System.out.print(c+" ");
-		a=b;b=c;
-} //конец цикла
- } //конец main()
-  } //êîíåö êëàññà
+		for (int i = 3; i < val; i++) {
+			c = a + b;
+			System.out.print(c + " ");
+			a = b;
+			b = c;
+		}
+	}
+}
